fa3172bfdc6645ffdc93c2b71074160a0c412186
N <you@mail.ua>
2016-04-21 18:21:06 +0300

    Try to add clusters for markers

--- sites/all/modules/ctrader_map/js/custom_map.js
+++ sites/all/modules/ctrader_map/js/custom_map.js
@@ -24,6 +24,73 @@
       L.geoJson(condosDataGeojson, {
         onEachFeature: onEachFeature
       }).addTo(mymap);
+
+
+
+
+      var drawnItems = new L.FeatureGroup();
+      mymap.addLayer(drawnItems);
+
+      var drawControl = new L.Control.Draw({
+        position: 'topright',
+        draw: {
+          polygon: {
+            shapeOptions: {
+              color: 'steelblue',
+              weight: 1,
+              opacity: .7,
+              dashArray: '20,3',
+              lineJoin: 'round'
+            },
+          },
+          polyline: {
+            shapeOptions: {
+              color: 'red'
+            },
+          },
+          rect: {
+            shapeOptions: {
+              color: 'green'
+            },
+          },
+          circle: {
+            shapeOptions: {
+              color: 'steelblue'
+            },
+          },
+        },
+        edit: {
+          featureGroup: drawnItems
+        }
+      });
+      mymap.addControl(drawControl);
+
+
+      var LeafIcon = L.Icon.extend({
+        options: {
+          shadowUrl:
+            'http://leafletjs.com/docs/images/leaf-shadow.png',
+          iconSize:     [38, 95],
+          shadowSize:   [50, 64],
+          iconAnchor:   [22, 94],
+          shadowAnchor: [4, 62],
+          popupAnchor:  [-3, -76]
+        }
+      });
+
+      var greenIcon = new LeafIcon({
+        iconUrl: 'http://leafletjs.com/docs/images/leaf-green.png'
+      });
+
+
+      mymap.on('draw:created', function (e) {
+        var type = e.layerType,
+          layer = e.layer;
+        drawnItems.addLayer(layer);
+      });
+
+
+
     }
   };
 
