11621a0402f5e47ebb17133c984041e9b3b154a8
Nikita <n.sineok@gmail.com>
2016-05-31 10:47:14 +0300

    Added block to search mini panel with hierarchical select for neighbourhoods vocabulary. Updated hierarchical_select module.

--- sites/all/modules/ctrader_saf/js/facets_script.js
+++ sites/all/modules/ctrader_saf/js/facets_script.js
@@ -10,4 +10,25 @@
     }
   };
 
+  Drupal.behaviors.regionHierarchical = {
+    attach: function (context) {
+      var parent = $('.neighbourhoods-hierarchical select:first-child option');
+      parent.each(function () {
+        if ($(this).attr('value') == 577 || $(this).attr('value') == 579) {
+          $(this).remove();
+        }
+
+        if ($(this).html().match("^grandparent_0") || $(this).html().match("^grandparent_5")) {
+          $(this).remove();
+        }
+      });
+
+      //  $('.neighbourhoods-hierarchical .form-select').chosen({
+      //    'disable_search': true
+      //  });
+      //
+      //  $('.neighbourhoods-hierarchical .selects').css('width', '200px');
+    }
+  };
+
 })(jQuery);
