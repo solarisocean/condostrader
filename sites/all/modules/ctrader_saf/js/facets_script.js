febe821c164a26fa216ebc4d81ad506139523843
Nikita <n.sineok@gmail.com>
2016-07-18 18:31:59 +0300

    Commented js for neighbourhoods taxonomy hierarchical select.

--- sites/all/modules/ctrader_saf/js/facets_script.js
+++ sites/all/modules/ctrader_saf/js/facets_script.js
@@ -17,29 +17,29 @@
 
   Drupal.behaviors.regionHierarchical = {
     attach: function (context) {
-      var parent = $('.neighbourhoods-hierarchical select:first-child option');
-      parent.each(function () {
-
-        // Hides all cities options except few and 'any' label.
-        var cities = ['Toronto', 'York', 'York Region', 'Peel', 'Halton'];
-        if($.inArray($(this).html(), cities) === -1) {
-          if($(this).val() != 'label_0') {
-            $(this).hide();
-          }
-        }
-      });
-
-      var taxOnUserRegister = $('.form-item.form-type-hierarchical-select.form-item-user-region-und select:first-child option');
-      taxOnUserRegister.each(function () {
-
-        // Hides all cities options except few and 'any' label.
-        var cities = ['Toronto', 'York', 'York Region', 'Peel', 'Halton'];
-        if($.inArray($(this).html(), cities) === -1) {
-          if($(this).val() != 'label_0') {
-            $(this).hide();
-          }
-        }
-      });
+      //var parent = $('.neighbourhoods-hierarchical select:first-child option');
+      //parent.each(function () {
+      //
+      //  // Hides all cities options except few and 'any' label.
+      //  var cities = ['Toronto', 'York', 'York Region', 'Peel', 'Halton', 'Durham'];
+      //  if($.inArray($(this).html(), cities) === -1) {
+      //    if($(this).val() != 'label_0') {
+      //      $(this).hide();
+      //    }
+      //  }
+      //});
+      //
+      //var taxOnUserRegister = $('.form-item.form-type-hierarchical-select.form-item-user-region-und select:first-child option');
+      //taxOnUserRegister.each(function () {
+      //
+      //  // Hides all cities options except few and 'any' label.
+      //  var cities = ['Toronto', 'York', 'York Region', 'Peel', 'Halton', 'Durham'];
+      //  if($.inArray($(this).html(), cities) === -1) {
+      //    if($(this).val() != 'label_0') {
+      //      $(this).hide();
+      //    }
+      //  }
+      //});
       // Hides third level on hierarchical select.
       //$('.neighbourhoods-hierarchical .selects select.form-select:nth-child(3)').remove();
     }
